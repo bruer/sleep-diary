46a31dcae99e2d4da42180866cd1d84a95a7e9cf
bruer <f.bruer@gmail.com>
2020-04-06 01:26:35 +0200

    some experimenting with dynamic style changes of the timer button

--- src/components/SleepSession/TimerButton.js
+++ src/components/SleepSession/TimerButton.js
@@ -1,14 +1,52 @@
 import React from "react";
 import { stopWatch } from "../../api/convertTime";
 import styles from "./TimerButton.module.css";
+import { useState } from "react";
 
 const TimerButton = ({ started, isOn, time, start, stop }) => {
+  const [border, setBorder] = useState({});
+  const [timerId, setTimerId] = useState(0);
+
   const handleClick = () => {
+    // if (isOn) {
+    //   stop();
+    //   stopAnimation();
+    // } else {
+    //   start();
+    //   startAnimation();
+    // }
     isOn ? stop() : start();
+  };
+  const getBorder = counter => {
+    const borders = [
+      { borderTop: "inset" },
+      { borderRight: "outset" },
+      { borderBottom: "outset" },
+      { borderLeft: "inset" }
+    ];
+    return borders[counter % 4];
+  };
+
+  const startAnimation = () => {
+    let n = 0;
+    setTimerId(
+      setInterval(() => {
+        setBorder(getBorder(++n));
+      }, 250)
+    );
+  };
+  const stopAnimation = () => {
+    clearInterval(timerId);
   };
 
   return (
-    <div onClick={handleClick} className={styles.container}>
+    <div
+      onClick={handleClick}
+      className={styles.container}
+      style={{
+        backgroundColor: isOn && time % 2 ? "blue" : "aquamarine"
+      }}
+    >
       <div className={`${styles.top} ${styles.letterspacing}`}>
         {started && !isOn ? "continue" : ""}
       </div>
@@ -22,11 +60,6 @@ const TimerButton = ({ started, isOn, time, start, stop }) => {
       <div className={`${styles.bottom} ${styles.letterspacing}`}>
         {started && isOn ? "pause" : ""}
       </div>
-
-      {/* <p className={`${styles.text} ${started ? styles.started : ""}`}>
-        {!started ? "start" : isOn ? "pause" : "continue"}
-      </p>
-      <p className={styles.counter}>{started && stopWatch(time)}</p> */}
     </div>
   );
 };
